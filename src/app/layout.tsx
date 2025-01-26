182712c373031d9e5573dda207336b9fe6a1c9ce
NouJouJin <108597930+NouJouJin@users.noreply.github.com>
2025-01-26 16:51:39 +0900

    ClientID変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,34 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThirdwebProvider } from "thirdweb/react"; // 本来は "@thirdweb-dev/react" が推奨
+import { ThirdwebProvider } from "thirdweb/react";
 import { Toaster } from "sonner";
 import { ToastProvider } from "@/components/ui/toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "NFT Minting template",
-  description: "A minting template powered by thirdweb",
+	title: "NFT Minting template",
+	description: "A minting template powered by thirdweb",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ToastProvider>
-          <Toaster position="bottom-center" />
-          <ThirdwebProvider
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<ToastProvider>
+					<Toaster position="bottom-center" />
+					<ThirdwebProvider
             clientId={process.env.THIRDWEB_CLIENT_ID} 
             // activeChain="polygon" など必要に応じて指定
           >
-            {children}
-          </ThirdwebProvider>
-        </ToastProvider>
-      </body>
-    </html>
-  );
+            {children}</ThirdwebProvider>
+				</ToastProvider>
+			</body>
+		</html>
+	);
 }
